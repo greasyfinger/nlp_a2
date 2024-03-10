ea7e7015e6cf042625bee99541e7af1285974f50
greasy finger <greasyfinger@greasy-Macbook.local>
2024-03-10 11:33:03 +0530

    done

--- task_2_torch/Bert_model.py
+++ task_2_torch/Bert_model.py
@@ -13,24 +13,24 @@ embeddings = BertModel.from_pretrained(model_name).embeddings
 
 
 # RNN model
-# print("RUNNING RNN")
-# class RNNSequenceTaggingModel(nn.Module):
-#     def __init__(self, embedding_dim, hidden_dim, num_layers, num_classes):
-#         super(RNNSequenceTaggingModel, self).__init__()
-#         self.embedding = embeddings
-#         self.rnn = nn.RNN(embedding_dim, hidden_dim, num_layers, batch_first=True)
-#         self.fc = nn.Linear(hidden_dim, num_classes)
-#         self.dropout = nn.Dropout(0.3)
+print("RUNNING RNN")
+class RNNSequenceTaggingModel(nn.Module):
+    def __init__(self, embedding_dim, hidden_dim, num_layers, num_classes):
+        super(RNNSequenceTaggingModel, self).__init__()
+        self.embedding = embeddings
+        self.rnn = nn.RNN(embedding_dim, hidden_dim, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_dim, num_classes)
+        self.dropout = nn.Dropout(0.3)
 
-#     def forward(self, input_ids):
-#         embeddings = self.embedding(input_ids)
-#         out, _ = self.rnn(embeddings)
-#         out = self.fc(self.dropout(out))
-#         return out
+    def forward(self, input_ids):
+        embeddings = self.embedding(input_ids)
+        out, _ = self.rnn(embeddings)
+        out = self.fc(self.dropout(out))
+        return out
 
 
-# model = RNNSequenceTaggingModel(768, 256, 1, num_labels)
-# run_epochs(model, tokenizer, "RNN-Bert_2")
+model = RNNSequenceTaggingModel(768, 256, 1, num_labels)
+run_epochs(model, tokenizer, "RNN-Bert_2")
 
 
 # LSTM model
